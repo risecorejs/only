2591332fce702c5a2a0f093ff3a403f3aeddae2e
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-03-29 15:41:16 +0600

    fix

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const only = require('./index')
 
 module.exports = () => (req, res, next) => {
   req.only = (...keys) => {
-    if (Array.isArray(keys)) {
+    if (Array.isArray(keys[0])) {
       keys = keys[0]
     }
 
